297bf4cd32235d887b11b4397064d54a2256ddd1
npankov <pankoff2000@gmail.com>
2022-03-21 16:56:56 +0100

    ticket-58. Edit my account adopter

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,21 +2,44 @@
 
 namespace App\Controller;
 
+use App\Entity\Adopter;
+use App\Form\AdopterType;
+use App\Repository\AdopterRepository;
+use Doctrine\ORM\OptimisticLockException;
+use Doctrine\ORM\ORMException;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AccountController extends AbstractController
 {
+    /**
+     * @throws OptimisticLockException
+     * @throws ORMException
+     */
     #[Route('/account', name: 'app_account')]
-    public function index(): Response
+    #[IsGranted('ROLE_ADOPTER')]
+    public function index(Request $request, AdopterRepository $adopterRepository): Response
     {
-        if (!$this->getUser()) {
-            return $this->redirectToRoute('app_login');
+        /** @var Adopter $adopter */
+        $adopter = $this->getUser();
+        $form = $this->createForm(AdopterType::class, $adopter, [
+            'method' => 'POST',
+        ]);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $adopterRepository->add($adopter);
+            $this->addFlash('success', 'Donnée insérée');
+            return $this->redirectToRoute('app_account');
         }
 
+
         return $this->render('account/index.html.twig', [
-            'controller_name' => 'AccountController',
+            'form' => $form->createView(),
         ]);
     }
 }
---
+++ src/Form/AdopterType.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Adopter;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AdopterType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email',
+                EmailType::class, // On peut lui donner un type (ici, on dit que c'est un input de type text
+                [
+                    'required' => true, // On passe une option, pour préciser que ce champ est requis (ne doit pas être vide)
+                ])
+            ->add('plainPassword',
+                TextType::class, // On peut lui donner un type (ici, on dit que c'est un input de type text
+                [
+                    'required' => true, // On passe une option, pour préciser que ce champ est requis (ne doit pas être vide)
+                ])
+            ->add('town',
+                TextType::class, // On peut lui donner un type (ici, on dit que c'est un input de type text
+                [
+                    'required' => true, // On passe une option, pour préciser que ce champ est requis (ne doit pas être vide)
+                ])
+            ->add('department',
+                TextType::class, // On peut lui donner un type (ici, on dit que c'est un input de type text
+                [
+                    'required' => true, // On passe une option, pour préciser que ce champ est requis (ne doit pas être vide)
+                ])
+            ->add('firstName',
+                TextType::class, // On peut lui donner un type (ici, on dit que c'est un input de type text
+                [
+                    'required' => true, // On passe une option, pour préciser que ce champ est requis (ne doit pas être vide)
+                ])
+            ->add('phone',
+                TelType::class, // On peut lui donner un type (ici, on dit que c'est un input de type text
+                [
+                    'required' => true, // On passe une option, pour préciser que ce champ est requis (ne doit pas être vide)
+                ])
+            ->add('lastName',
+                TextType::class, // On peut lui donner un type (ici, on dit que c'est un input de type text
+                [
+                    'required' => true, // On passe une option, pour préciser que ce champ est requis (ne doit pas être vide)
+                ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Valider'
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Adopter::class,
+        ]);
+    }
+}
